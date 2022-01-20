4eb39ab50aea16dedf21346bcff96f44eeed8609
Jimmy Chuk <84100848+ryjcm1@users.noreply.github.com>
2022-01-19 19:15:35 -0500

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,46 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalArr = transpose(letters);
+    const verticalJoin = verticalArr.map(ls => ls.join(''));
+    const backwards = backWardsArray(letters);
+    const backwardsVert = backWardsArray(verticalArr);
+    const backwardsHorizontalJoin = backwards.map(ls => ls.join(''));
+    const backwardsVerticalJoin = backwardsVert.map(ls => ls.join(''));
+  
+    return check(horizontalJoin, word) || check(verticalJoin, word) || check(backwardsHorizontalJoin, word) || check(backwardsVerticalJoin, word)
+  
+  };
+  
+  const backWardsArray = (letters) =>{
+    const backwards = letters.map(value => value.reverse());
+    return backwards;
+  };
+  
+  const check = (arr, word) =>{
+    for (l of arr) {
+      if (l.includes(word)) return true;
+    }
+    return false;
+  };
+  
+  const transpose = function(matrix) {
+    // Put your solution here
+    let newMatrix = [];
+  
+    const row = matrix[0].length;
+    const column = matrix.length;
+  
+    for (let i = 0; i < row; i++) {
+      const newRow = [];
+      for (let j = 0; j < column; j++) {
+        newRow.push(matrix[j][i]);
+      }
+      newMatrix.push(newRow);
+    }
+  
+    return newMatrix;
+  };
+  
+  module.exports = wordSearch;
+  
