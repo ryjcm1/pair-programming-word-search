26906ce56a8d83604e2248efd705c58eefd3a7c0
Jimmy Chuk <84100848+ryjcm1@users.noreply.github.com>
2022-01-19 19:16:10 -0500

    Update test_wordsearch.js

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontal", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,7 +31,55 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertical", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'F', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'R', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'N', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'K', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present backwards Horizontal", function() {
+    const result = wordSearch([
+      ['A', 'W', 'K', 'N', 'A', 'R', 'F', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'E', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'N', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'X', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present backwards Vertical", function() {
+    const result = wordSearch([
+      ['A', 'W', 'K', 'N', 'X', 'R', 'F', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'E', 'D'],
+      ['Y', 'F', 'C', 'F', 'K', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'N', 'V', 'N', 'G'],
+      ['W', 'H', 'C', 'S', 'A', 'E', 'X', 'L'],
+      ['B', 'F', 'R', 'E', 'R', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'F', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK');
 
     assert.isTrue(result);
   });
